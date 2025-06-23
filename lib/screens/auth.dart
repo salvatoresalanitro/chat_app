7141e9320f2fdad3763d597b3bcc25ff39947cc2
Salvo <salvatoresalanitro94@gmail.com>
2025-06-23 17:48:01 +0200

    initial layout for auth class

---
+++ lib/screens/auth.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Auth extends StatefulWidget {
+  const Auth({super.key});
+
+  @override
+  State<Auth> createState() => _AuthState();
+}
+
+class _AuthState extends State<Auth> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: 30,
+                  bottom: 20,
+                  left: 20,
+                  right: 20
+                ),
+                width: 200,
+                child: Image(image: image),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
