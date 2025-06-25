004bb2b2561b9241f9305826c02e6c65f2a681ab
Salvo <salvatoresalanitro94@gmail.com>
2025-06-25 09:04:58 +0200

    add new chat widget screen

---
+++ lib/screens/chat.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Chat extends StatelessWidget {
+  const Chat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FlutterChat'),
+      ),
+      body: const Center(
+        child: Text('Logged in!'),
+      ),
+    );
+  }
+}
