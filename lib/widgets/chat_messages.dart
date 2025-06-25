8ff49a936ec195eb5203b93cacabe1e765353675
Salvo <salvatoresalanitro94@gmail.com>
2025-06-25 16:03:09 +0200

    reverse the order of chat message

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -9,7 +9,7 @@ class ChatMessages extends StatelessWidget {
     return StreamBuilder(
       stream: FirebaseFirestore.instance
           .collection('chat')
-          .orderBy('createdAt', descending: false)
+          .orderBy('createdAt', descending: true)
           .snapshots(),
       builder: (ctx, chatSnapshot) {
         if (chatSnapshot.connectionState == ConnectionState.waiting) {
@@ -27,6 +27,8 @@ class ChatMessages extends StatelessWidget {
         final loadedMessages = chatSnapshot.data!.docs;
 
         return ListView.builder(
+          padding: const EdgeInsets.only(bottom: 40, left: 13, right: 13),
+          reverse: true,
           itemCount: loadedMessages.length,
           itemBuilder: (ctx, index) =>
               Text(loadedMessages[index].data()['text']),
