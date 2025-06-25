ce1ce153da3fa8f3754f2274836c4710c75f5659
Salvo <salvatoresalanitro94@gmail.com>
2025-06-25 17:16:02 +0200

    add cloud messagging to push and receive notification

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,11 +1,31 @@
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class Chat extends StatelessWidget {
+class Chat extends StatefulWidget {
   const Chat({super.key});
 
+  @override
+  State<Chat> createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    
+    setupPushNotifications();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
